86b894574274cf48d8f899b3ced1b970d6fed626
Yusril <yusrilchalif@gmail.com>
2021-01-27 13:59:35 +0700

    add source code

---
+++ Program/android version/app/src/main/java/com/example/ta_mask_detection/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.ta_mask_detection
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
