183519cb50af97ca2f2d75ac8aff3f68a304d491
Yusril <yusrilchalif@gmail.com>
2021-07-10 09:32:46 +0700

    fix apps

---
+++ Program/android version/app/src/main/java/com/example/ta_mask_detection/ARActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ta_mask_detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ARActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ar_activity)
+    }
+}
